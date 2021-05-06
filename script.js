5e3e0bb9c76fe06ad76a5fbc5cfc5e67e37470aa
NashVillarta <>
2021-05-06 14:34:10 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//  Listener for click
+document.getElementById('button').addEventListener('click', calculate)
+//  Ranges age to find appropiate age group
+function calculate () {
+  userage = document.getElementById('inputbox').value
+  userage = parseFloat(userage)
+  if (userage > 17) {
+    document.getElementById('textbox').innerHTML = 'You can view a R rated movie'
+  } else if (userage > 13) {
+    document.getElementById('textbox').innerHTML = 'You can view a PG 13 rated movie'
+  } else {
+    document.getElementById('textbox').innerHTML = 'You can view a G rated movie'
+  }
+}
